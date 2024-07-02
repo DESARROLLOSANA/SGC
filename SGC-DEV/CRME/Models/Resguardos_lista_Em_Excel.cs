7f08f089ce6353cf6c940a47d0945a033dcaf74c
SoporteSana <soporte@sana.com.mx>
2024-07-02 10:34:53 -0600

    Ultima Version (Macheada a SIRE)

--- SGC-DEV/CRME/Models/Resguardos_lista_Em_Excel.cs
+++ SGC-DEV/CRME/Models/Resguardos_lista_Em_Excel.cs
@@ -5,14 +5,13 @@ using System.Web;
 
 namespace CRME.Models
 {
-    public class Resguardos_lista_Em
+    public class Resguardos_lista_Em_Excel
     {
 		public int? Resguardo_Mobiliario_ID { get; set; }
 		public string Nombres { get; set; }
 		public string nombresco { get; set; }
 		public string entrega { get; set; }
-		public int Temporalidad { get; set; }
-		public int estado { get; set; }
-
+		public string Temporalidad { get; set; }
+		public string estado { get; set; }
 	}
 }
